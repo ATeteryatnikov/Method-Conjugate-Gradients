7e13f6097b4347b6f46b56a93b74d75fb3ac21b7
ATeteryatnikov <volk1695@mail.ru>
2017-01-19 21:00:42 +0700

    очистка

--- julia_conj/programms/тесты/без исп оператора */16 slides.pdf/iteracii.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-ABSOLUTE_FILENAME=`readlink -e "$0"`       # полный путь до исполняемого файла test.sh
-DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`   # берем только каталог в котором находится test.sh
-
-echo &(grep -i --recursive "количество" "$DIRECTORY") >> "$DIRECTORY/Iteracii.txt"
-
