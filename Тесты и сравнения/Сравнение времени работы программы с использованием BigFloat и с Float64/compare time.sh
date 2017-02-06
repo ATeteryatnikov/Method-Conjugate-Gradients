55f202192c06d57dc9129da59b742f0858a45915
ATeteryatnikov <volk1695@mail.ru>
2017-02-06 11:41:06 +0700

    Update compare time.sh

--- Тесты и сравнения/Сравнение времени работы программы с использованием BigFloat и с Float64/compare time.sh
+++ Тесты и сравнения/Сравнение времени работы программы с использованием BigFloat и с Float64/compare time.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#shell-скрипт "compare time.sh" служит для поиска строк, содержащих ключевое слово "timer: " во всех файлах в каталогах расположенных в одной директории со скриптом "compare time.sh":
+#Поиск производится только в каталогах со следующими путями:
+#/Kireev 16 slide with Float64/result
+#/Kireev 16 slide with BigFloat/result
+
+#Скрипт формирует текстовой файл "compare time.txt" содержащий найденные совпадения с ключевым словом вместе с путем до файла содержащего ключевое слово.
+
+
 
 ABSOLUTE_FILENAME=`readlink -e "$0"`       # полный путь до исполняемого файла test.sh
 DIRECTORY=`dirname "$ABSOLUTE_FILENAME"`   # берем только каталог в котором находится test.sh
